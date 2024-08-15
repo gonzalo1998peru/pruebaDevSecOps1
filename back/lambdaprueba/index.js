7ddd619610d7191ee68d2b41f3e6c7b9d7766775
gonzalo1998peru <91927212+gonzalo1998peru@users.noreply.github.com>
2024-08-14 19:46:03 -0500

    hola

---
+++ back/lambdaprueba/index.js
@@ -0,0 +1,17 @@
+exports.handler = async (event) => {
+    try {
+        // Lógica principal de tu Lambda
+        const response = {
+            statusCode: 200,
+            body: JSON.stringify('Esto es una prueba de despliegue de mi lambda 12/08/2024'),
+        };
+        return response;
+    } catch (error) {
+        // Manejo de errores
+        console.error('Error en la ejecución de la Lambda:', error);
+        return {
+            statusCode: 500,
+            body: JSON.stringify('Ocurrió un error en la Lambda'),
+        };
+    }
+};
